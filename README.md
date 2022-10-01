3dc14d5a39480b10da7ffd273fbffd628caee946
admin <admin@gmail.com>
2022-10-01 12:13:20 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# OYCountDownManager
+
+A description of this package.
---
+++ Tests/OYCountDownManagerTests/OYCountDownManagerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import OYCountDownManager
+
+final class OYCountDownManagerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(OYCountDownManager().text, "Hello, World!")
+    }
+}
